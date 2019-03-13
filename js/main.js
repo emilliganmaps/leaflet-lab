25960dbfaeb0d48a539fc412821768cc31bd78fd
Ellie Milligan <emilliganmaps@gmail.com>
2019-03-13 11:21:42 -0500

    Final Leaflet Lab
    Final Leaflet Lab

--- js/main.js
+++ js/main.js
@@ -36,8 +36,10 @@ function createSequenceControls(map, attributes){
                 var index = $(this).val();
             //update symbols as slider moves   
                 updatePropSymbols(map, attributes[index]);
+                updateLegend(map, attributes[index]);
             });
             
+            //click function to define how slider works
             $('.skip').click(function(){
                 //get the old index value
                 var index = $('.range-slider').val();
@@ -57,51 +59,132 @@ function createSequenceControls(map, attributes){
                 $('.range-slider').val(index);
                 //update symbols
                 updatePropSymbols(map, attributes[index]);
-            });     
+                updateLegend(map, attributes[index]);
+        });     
 };
 
+//function to create a dynamic map legend
 function createLegend(map, attributes){
-    var LegendControl = L.control.extend({
+    
+    var LegendControl = L.Control.extend({
         options: {
             position: 'bottomright'
         },
+
         onAdd: function (map) {
+            // create the control container with a particular class name
             var container = L.DomUtil.create('div', 'legend-control-container');
+
+            //add temporal legend div to container
             $(container).append('<div id="temporal-legend">')
+
+            //start attribute legend svg string
+            var svg = '<svg id="attribute-legend" width="300px" height="300px">';
             
-            var svg = 'svg id="attribute-legend" width="180px" height=180px">';
-            
+            //create values for placement of the circle lables in the legend
+            var circles = {
+                max: 110,
+                mean: 180,
+                min: 245
+            };
+
+            //loop to add each circle and text to svg string
+            for (var circle in circles){
+                //circle string
+                svg += '<circle class="legend-circle" id="' + circle + 
+                '" fill="#BA1125" fill-opacity="0.8" stroke="#A51126" cx="110"/>';
+                
+                svg += '<text id="' + circle + '-text" x="250" y="' + circles[circle] + '"></text>';
+            };
+
+            //close svg string
+            svg += "</svg>";
+
+            //add attribute legend svg to container
             $(container).append(svg);
             
-            $('.range-slider').on('input', function(){
-                var index = $(this).val();
-                updatePropSymbols(map, attributes[index]);
-                $(container).text(attributes[index]);
-            });
-            
-            $('.skip').click(function(){
-                var index = $('.range-slider').val();
-                if ($(this).attr('id') == 'forward'){
-                    index++;
-                    index = index > 14 ? 0 : index;
-                } else if ($(this).attr('id') == 'reverse'){
-                    index--;
-                    index = index < 0 ? 14 : index;
-                };
-                
-                updatePropSymbols(map, attributes[index]);
-                
-                $(container).text(attributes[index]);
-            });
+            //disable propagation from clicking on the legend
+            L.DomEvent.disableClickPropagation(container);
             return container;
         }
+        
     });
+
     map.addControl(new LegendControl());
+    
+    updateLegend(map, attributes[0]);
+            
 };
 
+//calculate the max, mean, and min values for a given attribute
+function getCircleValues(map, attribute){
+    //start with min at highest possible and max at lowest possible number
+    var min = Infinity,
+        max = -Infinity;
+
+    map.eachLayer(function(layer){
+        //get the attribute value
+        if (layer.feature){
+            var attributeValue = Number(layer.feature.properties[attribute]);
+
+            //test for min
+            if (attributeValue < min){
+                min = attributeValue;
+            };
+
+            //test for max
+            if (attributeValue > max){
+                max = attributeValue;
+            };
+        };
+    });
+
+    //set mean
+    var mean = (max + min) / 2;
+
+    //return values as an object
+    return {
+        max: max,
+        mean: mean,
+        min: min
+    };
+};
+
+//update the temporal legend with slider bar
+function updateLegend(map, attributes){
+    var year = attributes.split("_")[0];
+    //legend title
+    var content = "<strong>Pertussis Cases in " + year + "</strong>";
+    
+    //cycle the legend with the slider bar
+    $('#temporal-legend').html(content);
+    
+    //retrieves max/mean/min values
+    var circleValues = getCircleValues(map, attributes);
+    
+    for (var key in circleValues){
+        //get the radius
+        var radius = calcPropRadius(circleValues[key]);
+        
+        //nests the circles from the bottom up at a point
+        $('#'+key).attr({
+            cy: 250 - radius,
+            r: radius
+        });
+
+        //add legend text
+        $('#'+key+'-text').text(Math.round(circleValues[key]*100)/100 + " cases");
+    };
+    
+};
+
+//function to create popups instead of having popup definitions in multiple code blocks
 function createPopup(properties, attribute, layer, radius){
+    //defines the county
     var popupContent = "<p><b>County:</b> " + properties.county + "</p>";
+    //defines the year
     var year = attribute.split("_")[0];
+    //how the popup reads th number of pertussis cases
     popupContent += "<p><b>Number of reported pertussis cases in " + year + ":</b> " + properties[attribute];
     
     layer.bindPopup(popupContent, {
@@ -109,6 +192,7 @@ function createPopup(properties, attribute, layer, radius){
     });
 };
 
+//function for updating proportional symbols
 function updatePropSymbols(map, attribute){
     map.eachLayer(function(layer){
         if (layer.feature && layer.feature.properties[attribute]){
@@ -118,7 +202,8 @@ function updatePropSymbols(map, attribute){
             //update each feature's radius based on new attribute values
             var radius = calcPropRadius(props[attribute]);
             layer.setRadius(radius);
-
+            
+            //consolidates popup references
             createPopup(props, attribute, layer, radius);
         };
 	});
@@ -140,14 +225,14 @@ function pointToLayer(feature, latlng, attributes){
 	var attValue = Number(feature.properties[attribute]);
 	geojsonMarkerOptions.radius = calcPropRadius(attValue);
     
-    mainLayer = L.circleMarker(latlng, geojsonMarkerOptions);
+    layer = L.circleMarker(latlng, geojsonMarkerOptions);
 	
-	createPopup(feature.properties, attribute, mainLayer, geojsonMarkerOptions.radius);
+	createPopup(feature.properties, attribute, layer, geojsonMarkerOptions.radius);
 	
-	return mainLayer;		
+	return layer;		
 };
 		
-
+//process data
 function processData(data){
     //empty array to hold attributes
     var attributes = [];
@@ -157,7 +242,7 @@ function processData(data){
 
     //push each attribute name into attributes array
     for (var attribute in properties){
-        //only take attributes with population values
+        //only take attributes with real values
         if (attribute.indexOf("0") > -1){
             attributes.push(attribute);
         };
@@ -165,7 +250,8 @@ function processData(data){
 
     return attributes;
 };
-	
+
+//create proportional symbols
 function createPropSymbols(data, map, attributes){
 	//adjusts the symbols for each data point to reflect its value using the calcPropRadius function results
 	L.geoJson(data, {
@@ -177,7 +263,7 @@ function createPropSymbols(data, map, attributes){
 
 //function to retrieve the data and place it on the map
 function getData(map){
-    //load the data
+    //load the data from the pertussis json
     $.ajax("data/ca_pertussis.geojson", {
         dataType: "json",
         success: function(response){  
@@ -185,21 +271,10 @@ function getData(map){
             var attributes = processData(response);
                     createPropSymbols(response, map, attributes);
                     createSequenceControls(map, attributes);
+                    createLegend(map, attributes);
 		}
     });
 };
-
-//calculate the radius of each proportional symbol
-function calcPropRadius(attValue) {
-    //scale factor to adjust symbol size evenly
-    var scaleFactor = 15;
-    //area based on attribute value and scale factor
-    var area = attValue * scaleFactor;
-    //radius calculated based on area
-    var radius = Math.sqrt(area/Math.PI);
-
-    return radius;
-}; 
 
 //create a second layer of points for population data
 function getNextLayer(map){
@@ -238,11 +313,23 @@ function getNextLayer(map){
     });
 };
 
+//calculate the radius of each proportional symbol
+function calcPropRadius(attValue) {
+    //scale factor to adjust symbol size evenly
+    var scaleFactor = 15;
+    //area based on attribute value and scale factor
+    var area = attValue * scaleFactor;
+    //radius calculated based on area
+    var radius = Math.sqrt(area/Math.PI);
+
+    return radius;
+}; 
+
 //function call to create the leaflet map
 function createMap(){
     //zooms automatically to California
     var map = L.map('map', {
-        center: [36.7783, -119.4179],
+        center: [36.7783, -116.4179],
         zoom: 6
     });
 
@@ -260,47 +347,6 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=p
         getData(map);
         getNextLayer(map);
 };
-
-//Update the legend with new attribute
-function updateLegend(map, attribute){
-    //create content for legend
-    var year = attribute.split("_")[1];
-    var content = "Population in " + year;
-
-    //replace legend content
-    $('#temporal-legend').html(content);
-};
-
-function createLegend(map, attributes){
-    var LegendControl = L.Control.extend({
-        options: {
-            position: 'bottomright'
-        },
-
-        onAdd: function (map) {
-            // create the control container with a particular class name
-            var container = L.DomUtil.create('div', 'legend-control-container');
-
-            //add temporal legend div to container
-            $(container).append('<div id="temporal-legend">')
-
-            //Step 1: start attribute legend svg string
-            var svg = '<svg id="attribute-legend" width="180px" height="180px">';
-
-            //add attribute legend svg to container
-            $(container).append(svg);
-            
-            
-
-            return container;
-        }
-    });
-
-    map.addControl(new LegendControl());
-    
-    updateLegend(map, attributes[0]);
-};
-
 
 //make population a controllable layer
 function controlLayers(map){
